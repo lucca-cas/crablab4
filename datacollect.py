3c2f49cdec0fa9701248c6eeb42acfb291f4ad33
KhushK52 <126939956+KhushK52@users.noreply.github.com>
2024-04-25 04:39:08 +0000

    Update datacollect.py

--- datacollect.py
+++ datacollect.py
@@ -106,7 +106,7 @@ try:
 
             #now lets try to save the data 
 
-            np.savez(f'{file}point{point}', pol0 = first, pol1 = second, alt=alt, az=az, date = jd, missed = flops)
+            np.savez(f'{file}point{point}', pol0 = first, pol1 = second, ra = s_topos[i].ra, dec = s_topos[i].dec, alt=alt, az=az, date = jd, missed = flops)
         else:
             flops.update({point:[alt,az]})
             continue   
